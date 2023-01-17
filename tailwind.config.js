4c26aefbc58283b048c578ecbf2a2a6463b50d1c
Lakshman <lakshmanram2259@gmail.com>
2023-01-17 21:27:20 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.js",
-    "./pages/**/*.jsx",
-    "./components/**/*.jsx",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('tailwind-scrollbar-hide'),
+
   ],
 }
